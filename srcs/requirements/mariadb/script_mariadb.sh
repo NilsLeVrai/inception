22a90b59622edc14d48842bf3a164769abf49b17
NilsLeVrai <nilslucas@wanadoo.fr>
2025-06-25 15:34:18 +0200

    so close but yet so far

--- srcs/requirements/mariadb/script_mariadb.sh
+++ srcs/requirements/mariadb/script_mariadb.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
-service mariadb start
-kill $(cat /var/run/mysqld/mysqld.pid)
-exec mysqld
+
+chown -R mysql:mysql /var/lib/mysql
+chown -R mysql:mysql /run/mysqld
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+  echo "Initializing database..."
+  mysql_install_db --user=mysql --ldata=/var/lib/mysql
+fi
+
+mysqld_safe --datadir=/var/lib/mysql &
+sleep 5
+
+# Sécurité : s'assure que les variables sont définies
+if [ -n "$WORDPRESS_USER" ] && [ -n "$WORDPRESS_PASSWORD" ] && [ -n "$WORDPRESS_NAME" ]; then
+    echo "CREATE DATABASE IF NOT EXISTS ${WORDPRESS_NAME} ;" | mariadb
+
+    # Création utilisateur pour l'hôte 'wordpress' (nom du conteneur dans le réseau Docker)
+    echo "CREATE USER IF NOT EXISTS '${WORDPRESS_USER}'@'wordpress' IDENTIFIED BY '${WORDPRESS_PASSWORD}' ;" | mariadb
+    echo "GRANT ALL PRIVILEGES ON ${WORDPRESS_NAME}.* TO '${WORDPRESS_USER}'@'wordpress' ;" | mariadb
+
+    # En bonus : si besoin de compatibilité large
+    echo "CREATE USER IF NOT EXISTS '${WORDPRESS_USER}'@'%' IDENTIFIED BY '${WORDPRESS_PASSWORD}' ;" | mariadb
+    echo "GRANT ALL PRIVILEGES ON ${WORDPRESS_NAME}.* TO '${WORDPRESS_USER}'@'%' ;" | mariadb
+
+    echo "FLUSH PRIVILEGES;" | mariadb
+fi
+
+mysqladmin shutdown
+
+exec mysqld_safe --datadir=/var/lib/mysql
--- srcs/requirements/wordpress/script_wp.sh
+++ srcs/requirements/wordpress/script_wp.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-php-fpm7.4 -F
+set -ex
+cd /var/www/html
+
+if ! wp core is-installed --allow-root; then
+    rm -rf ./*
+    wp core download --allow-root
+    wp config create    --dbname="$WORDPRESS_NAME" \
+                        --dbuser="$WORDPRESS_USER" \
+                        --dbpass="$WORDPRESS_PASSWORD" \
+                        --dbhost="$WORDPRESS_HOST" \
+                        --allow-root
+    wp config set WP_CACHE true --raw --allow-root
+    wp core install --url="$DOMAIN_NAME" --title="$WORDPRESS_TITLE" \
+                --admin_user="$WORDPRESS_ADMIN_USER" \
+                --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+                --admin_email="$WORDPRESS_ADMIN_EMAIL" --allow-root
+    wp user create "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" --role=author --user_pass="$WORDPRESS_USER_PASSWORD" --allow-root
+    chown -R www-data:www-data /var/www/html
+fi
+cd;
+
+php-fpm7.4 -F --nodaemonize --fpm-config /etc/php/7.4/fpm/php-fpm.conf
